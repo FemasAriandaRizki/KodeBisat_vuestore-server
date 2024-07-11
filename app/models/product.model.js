6437af5764dbb7efb81de5fd64c54eadcc1225cb
femas arianda <femasariandarizki@students.undip.ac.id>
2024-07-11 23:18:19 +0700

    first commit

---
+++ app/models/product.model.js
@@ -0,0 +1,21 @@
+module.exports = mongoose => {
+    const Schema = mongoose.Schema(
+        {
+            code: String,
+            name: String,
+            price: Number,
+            description: String,
+            imageUrl: String,
+            averageRating: Number
+        }
+    )
+    
+    Schema.method("toJSON", function() {
+        const {__v, _id, ...object } = this.toObject()
+        object.id = _id;
+        return object
+    })
+
+    const Product = mongoose.model("products", Schema)
+    return Product
+}
